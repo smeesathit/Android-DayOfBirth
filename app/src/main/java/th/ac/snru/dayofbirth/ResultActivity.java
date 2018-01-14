b0bb5f6efba64564374cce7382beb1c1567af3e4
smeesathit <smeesathit.gmail.com>
2018-01-14 20:56:11 +0700

    ResultActivity - show day of birth.

--- app/src/main/java/th/ac/snru/dayofbirth/ResultActivity.java
+++ app/src/main/java/th/ac/snru/dayofbirth/ResultActivity.java
@@ -5,7 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class ResultActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -17,8 +23,42 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         Intent intent = getIntent();
         String dateString = intent.getStringExtra("date");
 
-        String message = "Input date = " + dateString;
-        Toast.makeText(getBaseContext(), message, Toast.LENGTH_SHORT).show();
+//        String message = "Input date = " + dateString;
+//        Toast.makeText(getBaseContext(), message, Toast.LENGTH_SHORT).show();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+
+
+        try {
+            Date date = sdf.parse(dateString);
+            SimpleDateFormat df = new SimpleDateFormat("EEEE");
+            TextView dowLabel = (TextView) findViewById(R.id.dowLabel);
+            TextView dowText = (TextView) findViewById(R.id.dowText);
+
+            dowText.setText(df.format(date));
+//        String message = "converted date:" +date.toString();
+//        Toast.makeText(getBaseContext(), message, Toast.LENGTH_LONG).show();
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+
+
+        } catch (ParseException e) {
+           // e.printStackTrace();
+            Toast.makeText(getBaseContext(), "Input Error", Toast.LENGTH_SHORT).show();
+        }
+
+//        String dateInString = "22-01-2015 10:20:56";
+/*
+        Date date = null;
+        try {
+            date = sdf.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+
+*/
         Button backButton = (Button)findViewById(R.id.backButton);
         backButton.setOnClickListener(this);
     }// End onCreate
